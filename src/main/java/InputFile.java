9e7937b8d053556f80319d983ef6e193add9a82f
johnsonjzs <43458107+johnsonjzs@users.noreply.github.com>
2020-06-08 23:14:20 +0800

    v0.11
    fix datatype bugs

--- src/main/java/InputFile.java
+++ src/main/java/InputFile.java
@@ -9,10 +9,11 @@ public class InputFile {
    * 讀取school_input.csv資料.
    * csv資料格式為:學校,國文權重,英文權重,數學權重,物理權重,化學權重
    *         型態：String,double,double,double,double,double
+ * @param schoolPath 
    * @return School[]
    * @throws Exception csv不存在，或資料格式錯誤
    */
-  public School[] readSchoolFile() throws Exception {
+  public School[] readSchoolFile(String schoolPath) throws Exception {
     ArrayList<School> arrayListSchools = new ArrayList<School>();
     FileInputStream fis = null;
     InputStreamReader isr = null;
@@ -61,10 +62,11 @@ public class InputFile {
    * 讀取student_input.csv資料.
    * csv資料格式為:姓名,國文,英文,數學,物理,化學,志願序1,志願序2,志願序3
    *         型態:String,int,int,int,int,int,String,String,String
+ * @param studentsPath 
    * @return Student[]
    * @throws Exception csv不存在，或資料格式錯誤
    */
-  public Student[] readStudentFile() throws Exception {
+  public Student[] readStudentFile(String studentsPath) throws Exception {
     ArrayList<Student> arrayListStudents = new ArrayList<Student>();
     FileInputStream fis = null;
     InputStreamReader isr = null;
